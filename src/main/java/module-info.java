97cde9e182544aa3c12069d418c3049249a85996
SebiB24 <sebycool242@gmail.com>
2025-02-19 01:25:17 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.date_medicale {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.date_medicale to javafx.fxml;
+    exports org.example.date_medicale;
+}
